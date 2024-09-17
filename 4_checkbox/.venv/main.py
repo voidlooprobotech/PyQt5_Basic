181ef2155222266faa4527849eb5a9c2cc9efff4
voidlooprobotech <voidlooprobotech@gmail.com>
2024-09-17 18:23:01 +0530

    added check box project

---
+++ 4_checkbox/.venv/main.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.checkbox = QCheckBox("Do you like food?", self)
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox.setGeometry(10,0,500,100)
+        self.checkbox.setStyleSheet("font-size: 30px;"
+                                    "font-family: Arial;")
+        self.checkbox.setChecked(False)
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+    def checkbox_changed(self, state):
+        print(state)
+        if state == Qt.Checked:
+            print("You like food")
+        else:
+            print("You dont like food")
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
